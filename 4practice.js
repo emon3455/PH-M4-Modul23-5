00e0cc37525c11ce092571cc897d1058b66f566a
emon3455 <emon.mhk69@gmail.com>
2023-02-03 23:16:56 +0600

    practice 4 comparison

---
+++ 4practice.js
@@ -0,0 +1,32 @@
+const x = 20;
+const y = 10;
+
+
+if(x > y){
+    console.log(x,"is greater then",y);
+}
+
+else if(x < y){
+    console.log(y,"is greater then",x);
+}
+
+else if (x === y){
+    console.log(y," is equal to ",x);
+}
+
+
+const a = 40;
+const b = 50;
+
+if(a >= b){
+    console.log (a,"greater then or equal to",b);
+}
+
+else if(a <= b){
+    console.log (a,"less then or equal to",b);
+}
+
+else if(a !== b){
+    console.log(a , "is not equal to",b);
+}
+
