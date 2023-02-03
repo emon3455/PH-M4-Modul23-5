e6b94b5374d7f16fb71fee02312aaa433cddb887
emon3455 <emon.mhk69@gmail.com>
2023-02-03 22:55:41 +0600

    practice task 1

---
+++ 1practice.js
@@ -0,0 +1,11 @@
+// different types of variables
+
+let text = "Emon";
+let isInRelation = false;
+let number = 10;
+let aray = [10,20,3,0,5,4,12];
+let student = {
+    name: "Emon",
+    cgpa: 3.82,
+    id: 211002117
+}
