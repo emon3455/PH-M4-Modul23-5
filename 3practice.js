df06c5353abfe05d72599e8790e0200c9a84244f
emon3455 <emon.mhk69@gmail.com>
2023-02-03 23:08:19 +0600

    practice 3 arithmetic operations

---
+++ 3practice.js
@@ -0,0 +1,26 @@
+// arithmetic operation:
+
+let num1 = 10;
+let num2 = 20;
+let num3;
+
+// addition:
+num3 = num1 + num2;
+console.log(num3);
+
+// substraction:
+num3 = num1 - num2;
+console.log(Math.abs(num3));
+
+// Multiplications:
+num3 = num1 * num2;
+console.log(num3);
+
+// Division:
+num3 = num1 / num2;
+console.log(num3);
+
+// Modulus:
+num3 = num1 % num2;
+console.log(num3);
+
