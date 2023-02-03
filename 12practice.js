b53511447ed394cbfd89648c8d4ea730055ae0b4
emon3455 <emon.mhk69@gmail.com>
2023-02-04 00:25:22 +0600

    practice task 12 done

---
+++ 12practice.js
@@ -0,0 +1,13 @@
+const person = {
+    name: " Emon ",
+    age: 21,
+    friends: ["Ridul","Sabbir","Adnan"],
+    cgpa: 3.82
+}
+
+console.log(person);
+
+person.age = 22;
+person.cgpa = 3.87;
+person.friends.push("Minhaz");
+console.log(person);
