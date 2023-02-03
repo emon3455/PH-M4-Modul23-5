37fef923390ea1333e167490ea4d8af22423d0e4
emon3455 <emon.mhk69@gmail.com>
2023-02-04 00:12:45 +0600

    problem 8 done abount array

---
+++ 8practice.js
@@ -0,0 +1,18 @@
+const age  = [10,20,30,40,50,60];
+console.log(age);
+
+// length of the array of elemnt is array
+console.log(age.length);
+
+// chnaging the 4th element of the array:
+age[3] = 100;
+console.log(age);
+
+// push element/ading:
+age.push(200);
+age.push(500);
+console.log(age);
+
+// pop element/ removing element:
+age.pop();
+console.log(age);
